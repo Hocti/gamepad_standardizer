21351bdcf3a4b16ba69fb6869b37c258f1eb1ece
hocti <>
2025-06-19 20:29:31 +0800

    pass

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import {HDpadMapping,SYSTEM_BUTTON_NAME,oppositeDpad} from './config'
 import {dpad,xy,dpadPress,directionWrap,getAnalogDirection,getDpadDirection} from './direction';
 import {SDLDB_setLink,SDLDB_fetch,SDLDB_processText,SDLDB_procesExtraText,addbtnNameProfile,getGamepadInfo,getDirectionAvailable,getDirection,getExtraAnalog,getButtonPress,getButtonValue,getButtonName} from './gamepad_standardizer';
 */
-export *  from './types';
-export *  from './config';
-export *  from './direction';
-export *  from './fixMapping';
-export *  from './gamepad_standardizer';
+export * from "./types";
+export * from "./config";
+export * from "./direction";
+export * from "./fixMapping";
+export * from "./gamepad_standardizer";
